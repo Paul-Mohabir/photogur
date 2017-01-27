36da7aac4099583d211497074eeaa070638f1f62
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-01-27 17:00:31 -0500

    fixed new feature to add a new picture

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,10 +9,17 @@ class PicturesController < ApplicationController
   end
 
   def new
+    @picture = Picture.new
   end
 
   def create
+    # make a new picture with what picture_params returns (which is a method we're calling)
     render text: "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
   end
 
 end
